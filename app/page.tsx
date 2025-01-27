26fadc04dca4c0595958fec5a90881934849dde2
Vmihira <vinjamurimihira@gmail.com>
2025-01-27 22:46:35 +0530

    updated charts and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import { NavbarDemo } from "@/components/Navbar_display";
 import { SparklesPreview2 } from "@/components/Sparkles_bg";
 import { AnimatedPinDemo } from "@/components/Pin";
 import { TimelineDemo } from "@/components/Steps_timeline";
+import { FloatingDockDemo } from "@/components/FloatingDockNavbar";
 
 export default function Home() {
 
   return (
     <main className="min-h-screen bg-gray-100">
-      <NavbarDemo />
+      <FloatingDockDemo />
       <SparklesPreview2 />
       <AnimatedPinDemo />
       <TimelineDemo />
